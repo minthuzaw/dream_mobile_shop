289ae9d43064c9fb48d71237997a4083b184d767
Thuta Min Thway <thutaminthway2001@gmail.com>
2022-04-28 11:50:16 +0630

    change email's auth to phone's auth

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <link rel="stylesheet" href="{{asset('css/style.css')}}">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
