f840e27f353b274da9d2987cf768ca9583e7779a
Min Thu Zaw <minthuzaw@Mins-MacBook-Air.local>
2022-05-15 10:10:04 +0630

    Add to cart done

--- app/Http/Livewire/Cart.php
+++ app/Http/Livewire/Cart.php
@@ -4,7 +4,6 @@ namespace App\Http\Livewire;
 
 use App\Models\Phone;
 use App\Models\PhoneUser;
-use http\Client\Curl\User;
 use Livewire\Component;
 
 class Cart extends Component
@@ -13,6 +12,7 @@ class Cart extends Component
     public $searchedPhones = [];
     public $itemCount = 0;
     public $phones = [];
+    public $total = 0;
 
     public function mount()
     {
@@ -21,27 +21,42 @@ class Cart extends Component
 
     protected function sync()
     {
-        $this->fetchItemCount();
         $this->fetchPhonesInCart();
+        $this->fetchItemCount();
+        $this->calculateTotal();
     }
 
-    protected function fetchItemCount()
-    {
-        $this->itemCount = PhoneUser::where('user_id', \Auth::id())->count();
+    public function clearSearch() {
+        $this->search = '';
+        $this->searchPhones($this->search);
     }
 
     protected function fetchPhonesInCart()
     {
-        $this->phones = PhoneUser::where('user_id', \Auth::id())->get();
+        $this->phones = \Auth::user()->phones;
+    }
+
+    protected function fetchItemCount()
+    {
+        $this->itemCount = count($this->phones);
+    }
+
+    protected function calculateTotal()
+    {
+        $this->total = 0;
+        foreach ($this->phones as $phone) {
+            $this->total += $phone->unit_price * $phone->pivot->quantity;
+        }
     }
 
     public function searchPhones($search)
     {
         if ($search) {
-            $this->searchedPhones = Phone::where('name', 'LIKE', '%' . $search . '%')->get();
+            $this->searchedPhones = Phone::where('name', 'LIKE', '%' . $search . '%')->limit(5)->get();
         } else {
             $this->searchedPhones = [];
         }
+        $this->sync();
     }
 
     public function addToCart($phoneId)
@@ -50,7 +65,7 @@ class Cart extends Component
 
         $phoneUser = PhoneUser::where('user_id', $userId)->where('phone_id', $phoneId)->first();
         if ($phoneUser) {
-            $phoneUser->update(['quantity' => ++$phoneUser->quantity]);
+            $this->checkStockAndIncreaseItem($phoneUser);
         } else {
             PhoneUser::create(['user_id' => $userId, 'phone_id' => $phoneId, 'quantity' => 1]);
         }
@@ -72,16 +87,23 @@ class Cart extends Component
     public function increaseItem($phoneId)
     {
         $phoneUser = PhoneUser::where('user_id', \Auth::id())->where('phone_id', $phoneId)->first();
-
-        // check if we have enough phones
-        $phoneUser->update(['quantity' => ++$phoneUser->quantity]);
-        $this->sync();
+        $this->checkStockAndIncreaseItem($phoneUser);
     }
 
     public function removeItem($phoneId)
     {
-        $phoneUser = PhoneUser::where('user_id', \Auth::id())->where('phone_id', $phoneId)->delete();
+        PhoneUser::where('user_id', \Auth::id())->where('phone_id', $phoneId)->delete();
         $this->sync();
+    }
+
+    public function checkStockAndIncreaseItem($phoneUser){
+        $phone = Phone::find($phoneUser->phone_id);
+        if ($phone->stock == $phoneUser->quantity){
+            return redirect()->route('cart')->with('error', 'Out of stock');
+        }else{
+            $phoneUser->update(['quantity' => ++$phoneUser->quantity]);
+            $this->sync();
+        }
     }
 
     public function render()
--- resources/views/livewire/cart.blade.php
+++ resources/views/livewire/cart.blade.php
@@ -1,21 +1,88 @@
-<div>
-    <input wire:change="searchPhones($event.target.value)" class="form-control">{{$itemCount}}
+<div class="position-relative">
 
-    <ul>
-        @foreach($searchedPhones as $phone)
-            <li>{{$phone->name}}
-                <button wire:click="addToCart({{$phone->id}})"><i class="fas fa-cart-plus"></i></button>
-            </li>
-        @endforeach
-    </ul>
+    <div class="d-flex">
+        <input wire:model="search" wire:change="searchPhones($event.target.value)" class="form-control mb-2 mr-4">
+        <button wire:click="clearSearch" class="btn btn-primary btn-sm form-control"><i class="fas fa-cross"></i></button>
+    </div>
 
+    @if(count($searchedPhones))
+        <div class="card card-body position-absolute bg-secondary text-white" style="left: 0; right: 0">
+            @foreach($searchedPhones as $phone)
+                <div class="border-bottom py-1 d-flex justify-content-between align-items-center">
+                    <img
+                        src="https://i.pravatar.cc/30?u={{ $phone->id }}"
+                        style="width: 30px; height: 30px"
+                        alt=""
+                        class="rounded-circle"
+                    />
+                    <p>{{$phone->model}}</p>
+                    <p>{{$phone->name}}</p>
+                    <p>{{$phone->unit_price}}</p>
+                    <button wire:click="addToCart({{$phone->id}})"><i class="fas fa-cart-plus"></i></button>
+                </div>
+            @endforeach
+        </div>
+    @endif
 
-    <ul>
+    <div class="d-flex flex-row-reverse my-3">
+        <b>Total : {{$total}}</b>
+    </div>
+    {{--    <ul>--}}
+    {{--        @foreach($searchedPhones as $phone)--}}
+    {{--            <li>{{$phone->name}}--}}
+    {{--                <button wire:click="addToCart({{$phone->id}})"><i class="fas fa-cart-plus"></i></button>--}}
+    {{--            </li>--}}
+    {{--        @endforeach--}}
+    {{--    </ul>--}}
+
+    <table class="table align-middle mb-0 bg-white ">
+        <thead class="bg-light">
+        <tr>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
         @foreach($phones as $phone)
-            <li>Phone / <button wire:click="decreaseItem({{$phone->phone_id}})"><i class="fas fa-minus"></i></button></li>
-            / {{$phone->quantity}}
-            /<button wire:click="increaseItem({{$phone->phone_id}})"><i class="fas fa-plus"></i></button></li>
-            /<button wire:click="removeItem({{$phone->phone_id}})"><i class="fas fa-trash"></i></button></li>
+            <tr>
+                <td>
+                    <img
+                        src="https://i.pravatar.cc/60?u={{ $phone->id }}"
+                        alt=""
+                        style="width: 45px; height: 45px"
+                        class="rounded-circle"
+                    />
+                </td>
+                <td>{{$phone->name}}</td>
+                <td>{{$phone->unit_price}}</td>
+                <td>{{optional($phone->pivot)->quantity}}</td>
+                <td>{{optional($phone->pivot)->quantity * $phone->unit_price}}</td>
+
+                <td>
+                    <button wire:click="decreaseItem({{$phone->id}})"><i class="fas fa-minus"></i></button>
+                    <button wire:click="increaseItem({{$phone->id}})"><i class="fas fa-plus"></i></button>
+                    <button wire:click="removeItem({{$phone->id}})"><i class="fas fa-trash"></i></button>
+                </td>
+            </tr>
         @endforeach
-    </ul>
+
+        </tbody>
+    </table>
+
+    {{--    <ul>--}}
+    {{--        @foreach($phones as $phone)--}}
+    {{--            <li>Phone /--}}
+    {{--                <button wire:click="decreaseItem({{$phone->phone_id}})"><i class="fas fa-minus"></i></button>--}}
+    {{--            </li>--}}
+    {{--            / {{$phone->quantity}}--}}
+    {{--            /--}}
+    {{--            <button wire:click="increaseItem({{$phone->phone_id}})"><i class="fas fa-plus"></i></button></li>--}}
+    {{--            /--}}
+    {{--            <button wire:click="removeItem({{$phone->phone_id}})"><i class="fas fa-trash"></i></button></li>--}}
+    {{--        @endforeach--}}
+    {{--    </ul>--}}
 </div>
