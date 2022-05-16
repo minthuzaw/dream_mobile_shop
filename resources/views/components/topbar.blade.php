ec3983c267f4d4dfc4312bc49c0dca795ed2ea52
Min Thu Zaw <minthuzaw775@gmail.com>
2022-05-16 20:39:06 +0630

    topbar modified

--- resources/views/components/topbar.blade.php
+++ resources/views/components/topbar.blade.php
@@ -177,18 +177,18 @@
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                     Profile
                 </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Settings
-                </a>
-                <a class="dropdown-item" href="#">
-                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Activity Log
-                </a>
+{{--                <a class="dropdown-item" href="#">--}}
+{{--                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>--}}
+{{--                    Settings--}}
+{{--                </a>--}}
+{{--                <a class="dropdown-item" href="#">--}}
+{{--                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>--}}
+{{--                    Activity Log--}}
+{{--                </a>--}}
                 <div class="dropdown-divider"></div>
-                <form action="{{route('logout')}}" method="POST" class="dropdown-item">
+                <form action="{{route('logout')}}" method="POST" class="dropdown-item text-center">
                     @csrf
-                    <button type="submit">
+                    <button type="submit" class="btn btn-outline-primary btn-sm">
                         <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                         Logout
                     </button>
