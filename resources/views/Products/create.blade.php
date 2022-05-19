15f3b94db9eacc446769c3067010a93f69b737f1
Min Thu Zaw <minthuzaw775@gmail.com>
2022-05-19 10:23:00 +0630

    phone create and edit blade modified

--- resources/views/Products/create.blade.php
+++ resources/views/Products/create.blade.php
@@ -6,60 +6,68 @@
 @endsection
 <div class="container">
     <div class="row">
-        <div class="col-12 text-center pt-2">
-            <form id="add-frm" method="POST" action="{{route('phones.store')}}" enctype="multipart/form-data" class="border p-3 mt-2 col-6">
+        <div class="col text-center pt-2">
+            <form id="add-frm" method="POST" action="{{route('phones.store')}}" enctype="multipart/form-data"
+                  class="border p-3 mt-2 col-6 bg-gray-200">
                 @csrf
-
-                <div class="control-group text-left">
-                    <label for="title">Model</label>
-                    <div>
-                        <input type="text" id="model" class="form-control mb-4" name="model"
-                               placeholder="Enter Phone Model"/>
+                <div class="row row-cols-2">
+                    <div class="col">
+                        <div class="control-group text-left">
+                            <label for="title">Model</label>
+                            <div>
+                                <input type="text" id="model" class="form-control mb-4" style="" name="model"
+                                       placeholder="Enter Phone Model"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="control-group  text-left">
+                            <label for="body">Name</label>
+                            <div>
+                                <input type="text" id="name" class="form-control mb-4" name="name"
+                                       placeholder="Enter Phone Name" rows=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="control-group  text-left">
+                            <label for="body">Stock</label>
+                            <div>
+                                <input type="number" id="stock" class="form-control mb-4" name="stock"
+                                       placeholder="Stocks count" rows=""/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="control-group text-left">
+                            <label for="image" class="">Select phone Image</label>
+                            <input type="file" name="image" class="form-control-file" id="image">
+                        </div>
                     </div>
                 </div>
-
-                <div class="control-group  text-left mt-2">
-                    <label for="body">Name</label>
-                    <div>
-                        <input type="text" id="name" class="form-control mb-4" name="name"
-                               placeholder="Enter Phone Name" rows=""/>
-                    </div>
-                </div>
-
-                <div class="control-group  text-left mt-2">
-                    <label for="body">Stock</label>
-                    <div>
-                        <input type="number" id="stock" class="form-control mb-4" name="stock"
-                               placeholder="Stocks count" rows=""/>
-                    </div>
-                </div>
-
-                <div class="control-group text-left mt-2">
-                    <label for="image" class="">Select phone Image</label>
-                    <input type="file" name="image" class="form-control-file" id="image">
-                </div>
-
-                <div class="control-group  text-left mt-2">
+                <div class="control-group  text-left ">
                     <label for="body">Select Brand</label>
-                    <select class="form-control form-select" aria-label="Default select example" name="brand_id">
+                    <select class="form-control form-select" aria-label="Default select example"
+                            name="brand_id">
                         <option disabled selected>Select Brand</option>
                         @foreach ($brands as $id => $brand)
                             <option value="{{$id}}">{{$brand}}</option>
                         @endforeach
                     </select>
                 </div>
-
-                <div class="control-group  text-left mt-2">
+                <div class="control-group  text-left ">
                     <label for="body">Price</label>
                     <div>
                         <input type="number" id="unit_price" class="form-control mb-4" name="unit_price"
                                placeholder="Enter Phone Price"/>
                     </div>
                 </div>
+
                 <div class="control-group  text-left mt-2">
                     <label for="body">Description</label>
                     <div>
-                        <textarea name="description" id="description" cols="58" rows="5"></textarea>
+                        <textarea name="description" id="description" class="form-control" cols="58"
+                                  rows="5"></textarea>
                     </div>
                 </div>
 
--- resources/views/Products/edit.blade.php
+++ resources/views/Products/edit.blade.php
@@ -6,40 +6,50 @@
 @endsection
 <div class="container">
     <div class="row">
-        <div class="col-12 text-center pt-2">
-            <form id="add-frm" method="POST" action="{{route('phones.update',$phone->id)}}" enctype="multipart/form-data"
-                  class="border p-3 mt-2 col-6">
+        <div class="col text-center pt-2">
+            <form id="add-frm" method="POST" action="{{route('phones.update',$phone->id)}}"
+                  enctype="multipart/form-data"
+                  class="border p-3 mt-2 col-6 bg-gray-200">
                 @csrf
                 @method('PUT')
-                <div class="control-group text-left">
-                    <label for="title">Model</label>
-                    <div>
-                        <input type="text" id="model" class="form-control mb-4" name="model"
-                               placeholder="Enter Phone Model" value="{!! $phone->model !!}" required>
+                <div class="row row-cols-2">
+                    <div class="col">
+                        <div class="control-group text-left">
+                            <label for="title">Model</label>
+                            <div>
+                                <input type="text" id="model" class="form-control mb-4" name="model"
+                                       placeholder="Enter Phone Model" value="{!! $phone->model !!}" required>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="control-group text-left mt-2">
-                    <label for="body">Name</label>
-                    <div>
-                        <input type="text" id="name" class="form-control mb-4" name="name"
-                               placeholder="Enter Phone Name" rows="" value="{!! $phone->name !!}" required></input>
+                    <div class="col">
+                        <div class="control-group text-left ">
+                            <label for="body">Name</label>
+                            <div>
+                                <input type="text" id="name" class="form-control mb-4" name="name"
+                                       placeholder="Enter Phone Name" rows="" value="{!! $phone->name !!}"
+                                       required></input>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="control-group text-left mt-2">
-                    <label for="body">Stock</label>
-                    <div>
-                        <input type="number" id="stock" class="form-control mb-4" name="stock"
-                               placeholder="Stocks count" rows="" value="{!! $phone->stock !!}" required></input>
+                    <div class="col">
+                        <div class="control-group text-left ">
+                            <label for="body">Stock</label>
+                            <div>
+                                <input type="number" id="stock" class="form-control mb-4" name="stock"
+                                       placeholder="Stocks count" rows="" value="{!! $phone->stock !!}"
+                                       required></input>
+                            </div>
+                        </div>
                     </div>
-                </div>
-
-                <div class="control-group text-left mt-2">
-                    <label for="image">Select phone Image</label>
-                    <input type="file" name="image" class="form-control-file" id="image">
-                    <div>
-                        <img src="{{asset('storage/'.$phone->image)}}" class="w-50" alt="">
+                    <div class="col">
+                        <div class="control-group text-left ">
+                            <label for="image">Select phone Image</label>
+                            <input type="file" name="image" class="form-control-file" id="image">
+                            <div>
+                                <img src="{{asset('storage/'.$phone->image)}}" class="w-50" alt="">
+                            </div>
+                        </div>
                     </div>
                 </div>
 
@@ -65,7 +75,7 @@
                 <div class="control-group  text-left mt-2">
                     <label for="body">Description</label>
                     <div>
-                        <textarea name="description" id="description" cols="58"
+                        <textarea name="description" id="description" class="form-control" cols="58"
                                   rows="5">{!! $phone->description !!}</textarea>
                     </div>
                 </div>
